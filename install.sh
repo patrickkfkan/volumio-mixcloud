8a9eb51b930ab933dbbb14181adfaf215ee37e02
patrickkfkan <patrickkfkan@gmail.com>
2024-11-13 21:28:00 +0800

    Prep for bookworm

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y python3 python3-pip python-libxslt1
-sudo pip3 install streamlink
+# On rpi, --force-overwrite is necessary to update the package.
+# Otherwise, apt will complain about overwriting "/usr/lib/python3.11/EXTERNALLY-MANAGED"
+# that was created by raspberrypi-sys-mods
+sudo apt install -y -o DPkg::options::="--force-overwrite" libpython3.11-stdlib
+sudo apt install -y streamlink
 echo "Mixcloud plugin installed"
 echo "plugininstallend"
